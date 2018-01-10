1602631a04df8df4300d0f92b3bb98b69af30b9c
Danny Straus <strausd@me.com>
2018-01-09 21:28:29 -0600

    Add coverage

--- src/tests/components/Header.test.jsx
+++ src/tests/components/Header.test.jsx
@@ -6,5 +6,6 @@ import Header from '../../components/Header';
 
 test('should render Header component properly', () => {
     const wrapper = shallow(<Header />);
+    
     expect(wrapper).toMatchSnapshot();
 });
